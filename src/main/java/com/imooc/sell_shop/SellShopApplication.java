b6428bba31ede41e7ef4d27d626ed118f801b68e
LiHuaWei00 <1293747294@qq.com>
2018-07-18 22:33:22 +0800

    spring-boot框架搭键

---
+++ src/main/java/com/imooc/sell_shop/SellShopApplication.java
@@ -0,0 +1,12 @@
+package com.imooc.sell_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellShopApplication.class, args);
+    }
+}
