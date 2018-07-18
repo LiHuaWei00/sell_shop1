c322d26a4b54489df21964bdc895cb54cfdab7d9
LiHuaWei00 <1293747294@qq.com>
2018-07-18 22:59:49 +0800

    此处测试一下子

--- src/main/java/com/imooc/sell_shop/SellShopApplication.java
+++ src/main/java/com/imooc/sell_shop/SellShopApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SellShopApplication {
-
+    //啦啦啦
     public static void main(String[] args) {
         SpringApplication.run(SellShopApplication.class, args);
     }
